c8d1940f99d2f371fead7507ff220c4259579646
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-04-16 03:30:44 -0300

    progress bar

--- js/script.js
+++ js/script.js
@@ -1,28 +1,90 @@
-function validateTextbox() {
+// function validateTextbox() {
+//
+//   if(confirm("Are you sure?")){
+//     var boxname = document.getElementById("name");
+//     var boxadd = document.getElementById("address");
+//     var boxphone = document.getElementById("phone");
+//     var boxemail = document.getElementById("email");
+//
+//     if(boxname.value == "" || boxadd.value == "" || boxphone.value == "" || boxemail.value == ""){
+//       alert("Fill out all the fields before submiting");
+//       return false;
+//     }
+//   }
+//   else {
+//     return false;
+//   }
+// }
+//
+// function goToGoogle() {
+//   window.location = "http://google.com";
+// }
+//
+// function addText(){
+//   var entry = document.getElementById("entry");
+//   var body = document.getElementById("place");
+//
+//   body.innerHTML = "asd";
+// }
 
-  if(confirm("Are you sure?")){
-    var boxname = document.getElementById("name");
-    var boxadd = document.getElementById("address");
-    var boxphone = document.getElementById("phone");
-    var boxemail = document.getElementById("email");
+function displayDate(date){
+    var datestr = date[2] + "-" + date[1] + "-" + date[0];
+    return datestr;
+}
 
-    if(boxname.value == "" || boxadd.value == "" || boxphone.value == "" || boxemail.value == ""){
-      alert("Fill out all the fields before submiting");
-      return false;
+function displayTime(date){
+  var datestr = (date[3] > 9 ?" ":" 0") + date[3] + (date[4] > 9 ?":":":0")+ date[4] + (date[5] > 9 ?":":":0") + date[5];
+  return datestr;
+}
+
+function dateDif(date1, date2){
+    var months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var diff = [0, 0, 0, 0, 0, 0]
+    for(var i=0; i<6; i++){
+      diff[i] = date2[i] - date1[i];
     }
-  }
-  else {
-    return false;
-  }
+    if(diff[1] < 0 && diff[0] > 0){
+      diff[0]--;
+      diff[1]+=12;
+    }
+    if(diff[2] < 0 && diff[1] > 0){ // if days < 0 and months > 0
+      diff[1]--; // month --
+      diff[2]+= months[(date2[1]-1) % 12] // add previous moth
+    }
+    return diff;
 }
 
-function goToGoogle() {
-  window.location = "http://google.com";
+function toSec(date){
+  var time = 0;
+  var secs = [365*24*60*60, 30*24*60*60, 24*60*60, 60*60, 60, 1];
+  for(var i =0; i<6; i++)
+    time += date[i] * secs[i];
+  return time;
 }
 
-function addText(){
-  var entry = document.getElementById("entry");
-  var body = document.getElementById("place");
+function countdown(){
 
-  body.innerHTML = "asd";
+  var d = new Date();
+
+  var startdate = [2019, 11, 24, 00, 00, 00];
+  var specteddate = [2020, 12, 21, 00, 00, 00];
+  var now = [d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds()];
+
+  var remaining = dateDif(now, specteddate);
+  var elapsed = dateDif(startdate, now);
+
+  var secstart = toSec(startdate), secspected = toSec(specteddate);
+  var percent = 100*(toSec(now) - secstart)/(secspected - secstart);
+
+  // document.getElementById("elapsed").innerHTML = elapsed[1] + " months      " + elapsed[2] + " days";
+  // document.getElementById("remaining").innerHTML = remaining[1] + " months      " + remaining[2] + " days";
+  
+  document.getElementById("timer").innerHTML = displayDate(now);
+  document.getElementById("progressbar1").style.width = percent + "%";
+  document.getElementById("progressbar1").innerHTML = elapsed[1] + " months      " + elapsed[2] + " days";
+  document.getElementById("progressbar2").style.width = (90 - percent) + "%";
+  document.getElementById("progressbar2").innerHTML = remaining[1] + " months      " + remaining[2] + " days";
 }
+
+
+setInterval(countdown, 500)
