e8d61297bb4db4c6c9fdac052f376266f853b291
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-04-27 17:15:30 -0300

    arrelgé el footer y agregué el texto de vuelve pato

--- test.sql
+++
@@ -1,8 +0,0 @@
-create table users (
-	user_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
-	user_usermane varchar(256) not null,
-	user_email varchar(256)
-);
-
-insert into users (user_usermane, user_email) values ('maxwell', 'maxwell@gmail.com');
-	insert into users (user_usermane, user_email) values ('pato', 'pato@gmail.com');
